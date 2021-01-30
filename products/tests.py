81e709bed1a48bf86b779033e58737d5fecca6d1
sue517 <csooa9305@gmail.com>
2021-01-31 04:37:25 +0900

    [16기 최수아] ProductGroupListView

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client
+from .models     import (
+    Product,
+    ProductGroup,
+    ProductGroupImage,
+    ProductGroupPackageType,
+    PackageType,
+    Category,
+    CategoryProduct
+)
+
+client = Client()
+
+class CategoryViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.category = Category.objects.create(
+            name = '반려동물 용품'
+        )
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_category_get_success:
+        response = client.get(f'/products/categories/{self.category.id}')
+        self.assertEqual(response.status_code, 200)
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+import re
+import json
+import uuid
+import boto3
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Count
+
+from .models          import (
+    Category,
+    CategoryProduct,
+    Product,
+    ProductGroup,
+    ProductGroupImage,
+    ProductGroupPackageType,
+    Relation
+)
+
+class CategoriesView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+
+        category = [{
+            "id" : category.id,
+            "name" : category.name
+        } for category in categories]
+
+        return JsonResponse({"category": category}, status=200)
+
+class ProductGroupListView(View):
+    def get(self, request):
+        page = int(request.GET.get('page', 1))
+
+        page_size = 16
+        limit     = int(page_size * page)
+        offset    = int(limit - page_size)
+
+        category  = request.GET.get('category', None)
+        search    = request.GET.get('search', None)
+        sort_type = request.GET.get('sort_type', None)
+
+        product_groups = ProductGroup.objects.order_by('id')
+
+        if category:
+            product_groups = product_groups.filter(categoryproduct__category_id=category)
+
+        if search:
+            product_groups = product_groups.filter(name__icontains=search)
+
+        if sort_type:
+            product_groups = product_groups.order_by(sort_type)
+
+        products = [{
+            'id'              : product_group.id,
+            'name'            : product_group.name,
+            'price'           : product_group.price,
+            'discount_rate'   : product_group.discount_rate,
+            'thumbnail'       : product_group.thumbnail,
+            'main_description': product_group.main_description
+        } for product_group in product_groups[offset:limit]]
+
+        return JsonResponse({"product_list": products}, status=200)
